e941b121cd2efa4e7c45554dc54edec6f36d3082
Manh Pham - PC <manhterry93@gmail.com>
2020-02-26 23:11:31 +0700

    First Init

---
+++ flutter_views/android/app/src/main/kotlin/pl/itto/flutter_views/MainActivity.kt
@@ -0,0 +1,12 @@
+package pl.itto.flutter_views
+
+import androidx.annotation.NonNull;
+import io.flutter.embedding.android.FlutterActivity
+import io.flutter.embedding.engine.FlutterEngine
+import io.flutter.plugins.GeneratedPluginRegistrant
+
+class MainActivity: FlutterActivity() {
+    override fun configureFlutterEngine(@NonNull flutterEngine: FlutterEngine) {
+        GeneratedPluginRegistrant.registerWith(flutterEngine);
+    }
+}
